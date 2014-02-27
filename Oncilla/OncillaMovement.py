6fcebf04927a6b078c57e22591632694ea2645c5
Pieter-Jan Van de Maele <pieterjanvandemaele@gmail.com>
2014-02-27 21:11:29 +0100

    Started working on QuadShift functionality

--- Oncilla/OncillaMovement.py
+++ Oncilla/OncillaMovement.py
@@ -4,9 +4,35 @@ import OncillaKinematics as OK
 import OncillaVisualization as OV
 import Oncilla2
 import time
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
 numpy.set_printoptions(precision=5)
 numpy.set_printoptions(suppress=True)
+
+def vector_intersection(v1,v2,d1,d2):
+    '''
+    v1 and v2 - Vector points
+    d1 and d2 - Direction vectors
+    returns the intersection point for the two vector line equations.
+    '''
+    if d1[0] == 0 and d2[0] != 0 or d1[1] == 0 and d2[1] != 0:
+        if d1[0] == 0 and d2[0] != 0:
+            mu = float(v1[0] - v2[0])/d2[0]
+        elif d1[1] == 0 and d2[1] != 0:
+            mu = float(v1[1] - v2[1])/d2[1]
+        return (v2[0] + mu* d2[0],v2[1] + mu * d2[1])
+    else:
+        if d1[0] != 0 and d1[1] != 0 and d2[0] != 0 and d2[1] != 0:
+            if d1[1]*d2[0] - d1[0]*d2[1] == 0:
+                raise ValueError('Direction vectors are invalid. (Parallel)')
+            lmbda = float(v1[0]*d2[1] - v1[1]*d2[0] - v2[0]*d2[1] + v2[1]*d2[0])/(d1[1]*d2[0] - d1[0]*d2[1])
+        elif d2[0] == 0 and d1[0] != 0:
+            lmbda = float(v2[0] - v1[0])/d1[0]
+        elif d2[1] == 0 and d1[1] != 0:
+            lmbda = float(v2[1] - v1[1])/d1[1]
+        else:
+            raise ValueError('Direction vectors are invalid.')
+        return (v1[0] + lmbda* d1[0],v1[1] + lmbda * d1[1])
 
 def Move_Leg(oncilla):
   # TODO
@@ -51,3 +77,35 @@ def Move_COB(oncilla, X_goal, q_init, q_ref, speed):
 
   return q_current
 
+def QuadShift(q_current, swingLeg):
+  # Move the body to position the COG inside the upcoming support polygon
+  stabilityMargin = 2.0
+
+  feet = OK.RelativeFootPositions(q_current) 
+  feet = numpy.delete(feet, 0, axis=0) # remove z-coordinates
+  print feet
+
+  if swingLeg == 1 or swingLeg == 4:
+    P1 = numpy.squeeze(numpy.asarray(feet[:, 1]))
+    P2 = numpy.squeeze(numpy.asarray(feet[:, 2]))
+
+  elif swingLeg == 2 or swingLeg == 3:
+    P1 = numpy.squeeze(numpy.asarray(feet[:, 0]))
+    P2 = numpy.squeeze(numpy.asarray(feet[:, 3]))
+
+  trot_dir = (P1-P2)/numpy.linalg.norm(P1-P2)
+ 
+  # Movement should be ortoghonal to trot line 
+  move_dir = numpy.array([trot_dir[1], -trot_dir[0]])
+
+  COB_start = numpy.array([0.0, 0.0])
+  intersection = vector_intersection(P1, COB_start, trot_dir, move_dir)
+
+  feet = numpy.delete(feet, swingLeg-1, axis=1)
+  fig, ax = plt.subplots()
+  ax.add_patch(Polygon(feet.T, closed=True, alpha=0.6))
+  ax.set_xlim([-200.0, 200.0])
+  ax.set_ylim([-100.0, 100.0])
+  ax.plot(intersection[0], intersection[1], 'ro')
+  ax.plot(0.0, 0.0, 'ko')
+  plt.show()
--- Test/Test_QuadShift.py
+++ Test/Test_QuadShift.py
@@ -16,15 +16,14 @@ numpy.set_printoptions(precision=4)
 
 # Oncilla hardware object
 oh = Oncilla2.OncillaHardware(simulation=True)
-oh.showSupportPolygon = True
 
-alpha1 = alpha2 = 10.0*math.pi/180.0
+alpha1 = alpha2 = 15.0*math.pi/180.0
 beta1 = beta2 = 135.0*math.pi/180.0
 gamma1 = gamma2 = 95.0*math.pi/180.0
 
-alpha3 = alpha4 = 10.0*math.pi/180.0
-beta3 = beta4 = 135.0*math.pi/180.0
-gamma3 = gamma4 = 95.0*math.pi/180.0
+alpha3 = alpha4 = 25.0*math.pi/180.0
+beta3 = beta4 = 115.0*math.pi/180.0
+gamma3 = gamma4 = 100.0*math.pi/180.0
 
 q_start = numpy.array([alpha1, beta1, gamma1, alpha2, beta2, gamma2, alpha3, beta3, gamma3, alpha4, beta4, gamma4])
 if oh is not None:
@@ -34,12 +33,4 @@ raw_input("Press key to continue")
 
 speed = 20.0
 
-X_goal = numpy.matrix([[20.0],[0.0],[0.0]])
-q = OM.Move_COB(oh, X_goal, q_start, q_start, speed)
-
-for i in range(0, 5):
-  X_goal = numpy.matrix([[20.0],[15.0],[15.0]])
-  q = OM.Move_COB(oh, X_goal, q, q_start, speed)
-
-  X_goal = numpy.matrix([[20.0],[-15.0],[-15.0]])
-  q = OM.Move_COB(oh, X_goal, q, q_start, speed)
+q = OM.QuadShift(q_start, swingLeg=1)
